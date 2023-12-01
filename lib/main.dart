b2234ea35be21d60c5b66d504c5fa9839f9c2f91
Gomab <egoiomab.com>
2023-12-01 09:08:33 -0800

    Essential Utilities, Helper Functions, Constants and much more

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,16 @@
 import 'package:diaspay_base/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
+/// -- Entry point of Flutter App
 void main() {
+
+  //Todo: Add Widgets Binding
+  //Todo: Init Local Storage
+  //Todo: Await Native Splash
+  //Todo: Initialize Firebase
+  //Todo: Initialize Authentication
+
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
