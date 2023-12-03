67e68a747cc1b844d9d237d4a82e340836dc8710
Gomab <egoiomab.com>
2023-12-03 14:39:00 -0800

    Onboarding Screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
+import 'package:diaspay_base/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:diaspay_base/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 /// -- Use this class to setup themes, initial Bindings, any animations and much
 class App extends StatelessWidget {
@@ -7,10 +9,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
